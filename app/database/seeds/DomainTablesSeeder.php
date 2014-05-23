0058be4be392e76c7efa36250cb644e0332b1bf0
Sallo Szrajbman <salloszrajbman@hotmail.com>
2014-05-23 16:34:29 -0700

    began domain tables seed

---
+++ app/database/seeds/DomainTablesSeeder.php
@@ -0,0 +1,16 @@
+<?php
+	class DomainTablesSeeder extends Seeder {
+		public function run(){
+			DB::table('privacies')->insert(array(
+											array('id'=>1, 'name'=>"Public"),
+											array('id'=>2, 'name'=>"Protected"),
+											array('id'=>3, 'name'=>"Private"),
+										));
+
+			DB::table('categories')->insert(array(
+											array('id'=>1, 'name'=>"Sports"),
+											array('id'=>2, 'name'=>"Politics"),
+											array('id'=>3, 'name'=>"Women"),
+										));
+		}
+}
