72c4d6d229e2b2301efb3da9c8e6066e1833f0d1
Sallo Szrajbman <salloszrajbman@hotmail.com>
2014-07-13 14:54:10 -0700

    image fixing localy

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -334,8 +334,8 @@ class AccountController extends BaseController {
 				->withErrors($validator)
 				->withInput();
 		} else {
-			$file = Input::file('avatar'); 
- 			$destinationPath = './public/uploads/'.sha1(Auth::user()->id);
+			$file = Input::file('avatar');
+ 			$destinationPath = 'public/uploads/'.sha1(Auth::user()->id);
 			$filename  = $file->getClientOriginalName();
 			
 		 	$upload_success = Input::file('avatar')->move($destinationPath, $filename);
